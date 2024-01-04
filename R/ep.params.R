fe19204d1c423df3d05d89fd513591e21f30eabf
popgengui <frankcuisine@gmail.com>
2024-01-03 21:41:15 -0700

    bug fix, malformed config entry for spatial migration's mean dispersal distance under single sex

--- R/ep.params.R
+++ R/ep.params.R
@@ -1119,7 +1119,7 @@ get.migr.spatial = function( g2sex, gnbpop, b.second.scheme )
 	if( g2sex == FALSE.AS.INT )
 	{
 
-		v.user.values = s.prompt.with.index.holder ( 
+		v.user.values = prompt.for.values.and.return.user.entries( 
 				"enter mean dispersal distance",
 				1, "numeric", c( 0, MAX_DISPERSAL_DISTANCE ) )
 
